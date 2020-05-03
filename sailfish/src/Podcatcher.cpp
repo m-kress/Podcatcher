b2611489b9fe79603a80c1d5dd06f98e1d578ff7
Moritz Carmesin <moritz@carmesinus.de>
2020-05-03 19:02:02 +0200

    Print stacktrace on crash

--- sailfish/src/Podcatcher.cpp
+++ sailfish/src/Podcatcher.cpp
@@ -4,6 +4,25 @@
 #include <sailfishapp.h>
 #include "podcatcherui.h"
 
+
+#include <execinfo.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void handler(int sig) {
+  void *array[100];
+  size_t size;
+
+  // get void*'s for all entries on the stack
+  size = backtrace(array, 100);
+
+  // print out all the frames to stderr
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
+
 int main(int argc, char *argv[])
 {
     // SailfishApp::main() will display "qml/template.qml", if you need more
@@ -14,6 +33,9 @@ int main(int argc, char *argv[])
     //   - SailfishApp::pathTo(QString) to get a QUrl to a resource file
     //
     // To display the view, call "show()" (will show fullscreen on device).
+
+        signal(SIGSEGV, handler);   // install our handler
+
 
     QGuiApplication* app = SailfishApp::application(argc,argv);
 
